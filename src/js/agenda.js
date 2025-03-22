0a2042afd90f8c73a39a9f0523410a921e2f24f4
Brendan Fleurdelys <fbrend23@gmail.com>
2025-03-22 19:16:26 +0100

    fix(agenda) Correction des attributions de numéro pour les jours

--- src/js/agenda.js
+++ src/js/agenda.js
@@ -8,6 +8,8 @@ import { Module } from "./module.js";
 // la semaine. Chaque module est défini par une instance de la classe Module,
 // qui précise le jour de la semaine ainsi que les horaires.
 // ---------------------------------------------------------------------------------
+// Les jours de la semaine sont numérotés de 0 (dimanche) à 6 (samedi).
+// ---------------------------------------------------------------------------------
 // Exemple :
 //   - Pour le lundi (dayOfWeek = 1)
 //   - Pour le mardi (dayOfWeek = 2) etc...
@@ -31,51 +33,52 @@ export const weeklySchedule = [
 // --------------------------
 // Modules du mardi (dayOfWeek = 2)
 // --------------------------
-  new Module("I322", "B11", 1, 8, 0, 8, 45),
-  new Module("I322", "B11", 1, 8, 50, 9, 35),
-  new Module("I322", "B11", 1, 9, 50, 10, 35),
-  new Module("I322", "B11", 1, 10, 40, 11, 25),
+  new Module("I322", "B11", 2, 8, 0, 8, 45),
+  new Module("I322", "B11", 2, 8, 50, 9, 35),
+  new Module("I322", "B11", 2, 9, 50, 10, 35),
+  new Module("I322", "B11", 2, 10, 40, 11, 25),
 
-  new Module("I122", "A11", 1, 13, 10, 13, 55),
-  new Module("I122", "A11", 1, 14, 0, 14, 45),
-  new Module("I122", "A11", 1, 15, 0, 15, 45),
-  new Module("I122", "A11", 1, 15, 50, 16, 35),
+  new Module("I122", "A11", 2, 13, 10, 13, 55),
+  new Module("I122", "A11", 2, 14, 0, 14, 45),
+  new Module("I122", "A11", 2, 15, 0, 15, 45),
+  new Module("I122", "A11", 2, 15, 50, 16, 35),
   
 // --------------------------
 // Modules du mercredi (dayOfWeek = 3)
 // --------------------------
-new Module("Sport", "Dojo", 1, 9, 50, 10, 35),
-new Module("Sport", "Dojo", 1, 10, 40, 11, 25),
+new Module("Sport", "Dojo", 3, 9, 50, 10, 35),
+new Module("Sport", "Dojo", 3, 10, 40, 11, 25),
 
-new Module("P_Prod", "N509", 1, 12, 20, 13, 5),
-new Module("P_Prod", "N509", 1, 13, 10, 13, 55),
-new Module("P_Prod", "N509", 1, 14, 0, 14, 45),
-new Module("P_Prod", "N509", 1, 15, 0, 15, 45),
+new Module("P_Prod", "N509", 3, 12, 20, 13, 5),
+new Module("P_Prod", "N509", 3, 13, 10, 13, 55),
+new Module("P_Prod", "N509", 3, 14, 0, 14, 45),
+new Module("P_Prod", "N509", 3, 15, 0, 15, 45),
 
 // --------------------------
 // Modules du jeudi (dayOfWeek = 4)
 // --------------------------
-new Module("I254", "A21", 1, 8, 0, 8, 45),
-new Module("I254", "A21", 1, 8, 50, 9, 35),
-new Module("I254", "A21", 1, 9, 50, 10, 35),
-new Module("I254", "A21", 1, 10, 40, 11, 25),
+new Module("I254", "A21", 4, 8, 0, 8, 45),
+new Module("I254", "A21", 4, 8, 50, 9, 35),
+new Module("I254", "A21", 4, 9, 50, 10, 35),
+new Module("I254", "A21", 4, 10, 40, 11, 25),
 
-new Module("I322", "B11", 1, 12, 20, 13, 5),
-new Module("I322", "B11", 1, 13, 10, 13, 55),
-new Module("I322", "B11", 1, 14, 0, 14, 45),
-new Module("I322", "B11", 1, 15, 0, 15, 45),
+new Module("I322", "B11", 4, 12, 20, 13, 5),
+new Module("I322", "B11", 4, 13, 10, 13, 55),
+new Module("I322", "B11", 4, 14, 0, 14, 45),
+new Module("I322", "B11", 4, 15, 0, 15, 45),
 
 // --------------------------
 // Modules du Vendredi (dayOfWeek = 5)
 // --------------------------
-new Module("C107", "A02", 1, 8, 0, 8, 45),
-new Module("C107", "A02", 1, 8, 50, 9, 35),
-new Module("C107", "A02", 1, 9, 50, 10, 35),
-new Module("C107", "A02", 1, 10, 40, 11, 25),
-new Module("C107", "A02", 1, 11, 30, 12, 15),
+new Module("C107", "A02", 5, 8, 0, 8, 45),
+new Module("C107", "A02", 5, 8, 50, 9, 35),
+new Module("C107", "A02", 5, 9, 50, 10, 35),
+new Module("C107", "A02", 5, 10, 40, 11, 25),
+new Module("C107", "A02", 5, 11, 30, 12, 15),
 
-new Module("I254", "A21", 1, 13, 10, 13, 55),
-new Module("I254", "A21", 1, 14, 0, 14, 45),
-new Module("I254", "A21", 1, 15, 0, 15, 45),
-new Module("I254", "A21", 1, 15, 50, 16, 35),
+new Module("I254", "A21", 5, 13, 10, 13, 55),
+new Module("I254", "A21", 5, 14, 0, 14, 45),
+new Module("I254", "A21", 5, 15, 0, 15, 45),
+new Module("I254", "A21", 5, 15, 50, 16, 35),
 ];
+
